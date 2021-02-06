961aea3fd807d8eb2970e5eae5f1b99e8e0152f5
LaisLimaSantos <74927227+LaisLimaSantos@users.noreply.github.com>
2021-02-06 05:52:44 +0300

    Add files via upload

--- BlogPessoal/src/main/java/org/generation/BlogPessoal/model/Postagem.java
+++ BlogPessoal/src/main/java/org/generation/BlogPessoal/model/Postagem.java
@@ -6,14 +6,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+
 @Entity
-@Table (name = "tb_postagem")
+@Table (name = "postagem")
+
 public class Postagem {
 	
 	@Id
@@ -21,57 +27,50 @@ public class Postagem {
 	private long id;
 	
 	@NotNull
-	private String nome;
-	
-	@NotNull
 	@Size(min = 5, max = 100)
 	private String titulo;
 	
 	@NotNull
-	@Size(min = 10, max = 1000)
-	private String texto;	
-
+	@Size(min = 10, max = 500)
+	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	public long getId() {
 		return id;
 	}
-
 	public void setId(long id) {
 		this.id = id;
 	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
 	public String getTitulo() {
 		return titulo;
 	}
-
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
-
 	public String getTexto() {
 		return texto;
 	}
-
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
-
 	public Date getData() {
 		return data;
 	}
-
 	public void setData(Date data) {
 		this.data = data;
 	}
-
-	@Temporal(TemporalType.TIMESTAMP)
-    private Date data = new java.sql.Date(System.currentTimeMillis());
-
+	
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
 }
--- BlogPessoal/src/main/java/org/generation/BlogPessoal/repository/PostagemRepository.java
+++ BlogPessoal/src/main/java/org/generation/BlogPessoal/repository/PostagemRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.generation.BlogPessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PostagemRepository  extends JpaRepository<Postagem, Long >{
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 	
-	public List<Postagem> findAllByNomeContainingIgnoreCase(String nome); 
-
+	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	
 }
